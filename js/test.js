a6e49f094dcb76e226c6ef0628379ad2c4317e3e
AshwaniTj <ashwani.tejan56@gmail.com>
2021-05-11 19:32:10 +0530

    created btnfunctioin

--- js/test.js
+++ js/test.js
@@ -1,3 +1,31 @@
-function cross(){
-	document.write(0);
+function button1click(){
+	var btn = document.getElementById("box1");
+    btn.innerHTML = '0';
+}
+function button2click(){
+	var btn = document.getElementById("box2");
+    btn.innerHTML = '0';
+}
+function button3click(){
+	var btn = document.getElementById("box3");
+    btn.innerHTML = '0';
+}
+function button4click(){
+	var btn = document.getElementById("box4");
+    btn.innerHTML = '0';
+}function button5click(){
+	var btn = document.getElementById("box5");
+    btn.innerHTML = '0';
+}function button6click(){
+	var btn = document.getElementById("box6");
+    btn.innerHTML = '0';
+}function button7click(){
+	var btn = document.getElementById("box7");
+    btn.innerHTML = '0';
+}function button8click(){
+	var btn = document.getElementById("box8");
+    btn.innerHTML = '0';
+}function button9click(){
+	var btn = document.getElementById("box9");
+    btn.innerHTML = '0';
 }
